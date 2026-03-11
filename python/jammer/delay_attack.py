b1fe8007748995eb6f81b3c232b284bd71540c68
gershylevy <kingershy@gmail.com>
2026-03-11 16:15:25 +0200

    fixed delay block

--- python/jammer/delay_attack.py
+++ python/jammer/delay_attack.py
@@ -6,7 +6,7 @@
 # SPDX-License-Identifier: GPL-3.0-or-later
 #
 
-# delay = 20,000 in 1 MHz -> 20 ms delay
+# delay = 0.02 at 1 MHz sample rate -> 20 ms delay (20,000 samples)
 
 
 import numpy as np
@@ -21,26 +21,25 @@ class delay_attack(gr.sync_block):
     and the next call will be
     [D E . . .]
     """
-    def __init__(self, f1=400,f2=400,delay=1024):
+    def __init__(self, f1=400, f2=400, delay=0.001, sample_rate=1e6):
         gr.sync_block.__init__(self,
             name="delay_attack",
             in_sig=[np.complex64],
             out_sig=[np.complex64])
 
-        self.delay = int(delay)
-        self.buffer = np.zeros(int(delay), dtype=np.complex64)
+        delay_samples = int(delay * sample_rate)
+        self.delay = delay_samples
+        self.buffer = np.zeros(delay_samples, dtype=np.complex64)
         self.index = 0
 
     def work(self, input_items, output_items):
         in0 = input_items[0]
         out = output_items[0]
+        N = len(in0)
 
-        for i in range(len(in0)):
-            out[i] = self.buffer[self.index]
-            self.buffer [self.index] = in0[i]
+        indices = (self.index + np.arange(N)) % self.delay
+        out[:] = self.buffer[indices]
+        self.buffer[indices] = in0
+        self.index = (self.index + N) % self.delay
 
-            self.index += 1
-            if self.index >= self.delay:
-                self.index = 0
-
-        return len(output_items[0])
+        return N
